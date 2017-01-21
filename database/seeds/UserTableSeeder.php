ae6a484b0dc9ffdc0e7f824ef022f99131d0d94c
Seven <shiweidu@outlook.com>
2017-01-21 09:51:17 +0000

    Apply fixes from StyleCI

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -5,8 +5,10 @@ use Zank\Database\Seed\Seeder;
 class UserTableSeeder extends Seeder
 {
     protected function handle(bool $reset)
-    {}
+    {
+    }
 
     protected function reset()
-    {}
+    {
+    }
 }
